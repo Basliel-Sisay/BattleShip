f2818f381df240ce1f4326247020f9a87283a828
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-11-07 18:14:45 +0300

    tested the game.js logic with jest

---
+++ test/game.spec.js
@@ -0,0 +1,13 @@
+import {turn , gameReset} from '../src/game.js';
+import {Ship} from '../src/ship.js';
+import { Gameboard} from '../src/gameboard.js';
+import {player, computer} from '../src/Player.js';
+test('test if the current player is player when the game starts', function(){
+    expect(gameReset(player, computer, 'current')).toBe('player');
+});
+test('test if when the game reset\'s there is a new board created' , function(){
+    expect(computer.board).toBeTruthy();
+});
+test('test if computer is not the stating player.', function(){
+    expect(gameReset(player, computer, 'current')).not.toBe('computer');
+});
