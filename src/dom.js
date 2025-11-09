cfe284de5a70eb580031046124f12d934aeaeb58
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-11-09 21:46:57 +0300

    Added board display function with game state visualization

---
+++ src/dom.js
@@ -0,0 +1,28 @@
+import { Gameboard } from "./gameboard.js";
+function display(board, show){ // board is the instance of Gameboard that got like the game state
+    if(show === undefined){// show is the variable that shows ships basically it is true for player board and false for the computer board
+        show = true; //the ships will be visisble
+    }
+    const container = document.querySelector('#gameHolder');//this is just what we will have in our html file in the future
+    container.innerHTML =''; // clear our container to not overlap or stack many boards on top of each other we will clear it
+    for(let row = 0; row<10;row++){// loop through the grid
+        for(let column =0; column<10;column++){
+            const box = document.createElement('div');// a div container that represent one box on the board
+            box.classList.add('box'); // this is added to later style it in our css file
+            box.dataset.row = row; // we will attach data-row and column attributes to the box like (<div class="box" data-row="3" data-col="7"></div>)
+            box.dataset.column=column;
+            const gotAttacked = board.allAttack.some(place => place.row ===row && place.column === column); // check if the box is attacked and it will return true if it does
+            const gotMissed = board.missed.some(place => place.row === row && place.column === column); // check if the box is missed
+            const gotShip = board.grid[row][column] !== null; //checks if there is a ship
+            if(gotAttacked && gotShip){
+                box.classList.add('hit'); // if the box is attacked and got ship innit make it hit
+            }else if(gotMissed){
+                box.classList.add('miss');// if not make it miss
+            }
+            else if(show == true && gotShip){ // if ship is present show the ship and the computer won't have this because we don't want to know where the ships are at unless we tried to attack it and actually got it
+                box.classList.add('ship');
+            }
+            container.appendChild(box);
+        }
+    }
+}
