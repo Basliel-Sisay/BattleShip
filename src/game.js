baa87f5300e804379e6acf8e1d854d79fc8b60fd
braveset14 <braveset14@gmail.com>
2025-11-10 12:51:59 +0300

    Updated Turn function to take a callback function.

--- src/game.js
+++ src/game.js
@@ -2,31 +2,36 @@ import { Gameboard} from "./gameboard";
 import { Ship } from "./ship.js";
 import {player, computer} from './Player.js';
 computer.placement();
-let current = 'player';
-export function turn(row, column){
+// I exported current because we use it in dom.js.
+export let  current = 'player';
+// Changed turn function to accept a callback function that takes 'player' if plaer won
+//'computer' if computer won and null if the game continues because thia is what we will use to check gameOver in dom.js.
+//our previous turn() function returned from inside of the setTimeOut() which causes error.
+
+export function turn(row, column,callback){
     const final = player.attack(computer , row, column);
     if(computer.board.gameOver()=== true){
-        console.log('the computer loses');
+        callback('player');
         return;
     }
-    if(current === 'player'){
+    
        current = 'computer'; 
-    }
+
    setTimeout(() => {if(player.board.gameOver()!==true && current ==='computer' ){
         computer.attack(player);
         current = 'player';
         if(player.board.gameOver()===true){
-            console.log('the computer wins');
+            callback('computer');
+        }else{
+            callback(null);
         }
     }}, 1000); 
 }
-
-export function gameReset(player, computer, current){
+// gameReset used to take parameters but now it doesnt needd them it simply resets the objects imported.
+export function gameReset(){
     computer.board = new Gameboard();
     computer.attacked = [];
     computer.placement();
     player.board= new Gameboard();
     current = 'player';
-    return current;
-
 }
