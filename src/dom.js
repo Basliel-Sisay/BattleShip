5ed58534f77bb25a4921792957141f277ffb4426
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-11-10 09:48:51 +0300

    Added game movement logic with attack validation and multiple board rendering

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,6 @@
 import { Gameboard } from "./gameboard.js";
+import { computer, player } from "./Player";
+import { turn } from "./game.js";
 function display(board, show){ // board is the instance of Gameboard that got like the game state
     if(show === undefined){// show is the variable that shows ships basically it is true for player board and false for the computer board
         show = true; //the ships will be visisble
@@ -25,4 +27,24 @@ function display(board, show){ // board is the instance of Gameboard that got li
             container.appendChild(box);
         }
     }
+}
+function movement(row , column){
+    const gotAttacked = computer.board.allAttack.some(place => place.row === row && place.column === column); // check if the computer board is already got attacked and it will return true if does got attacked
+    if(gotAttacked === true){
+        return; //if the board is already got attacked return nothing to avoid same attack
+    }
+    turn(row , column); // now we can call turn from game.js to handle game turn logic because like we imported it at the start of the dom.js file
+    const boards =[ // this is an array object that have the displaye parameters as an argument passed since we imported the gameboard
+    //  we can assign the property board to a player and computer board and decide to hide the computer ships and show the player ships(in our POV) 
+    // then loop throguh it to access everything
+        {
+            board: player.board,
+            show: true
+        },
+        {
+            board : computer.board,
+            show: false
+        }
+    ]
+    boards.forEach(access=> display(access.board , access.show));// access it through calling the display function which contain board and show parameters
 }
